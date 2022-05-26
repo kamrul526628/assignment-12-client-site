d79e556751dd05a855493c35bb6107e640b9ba02
kamrul526628 <kamrulhasan526628@hmail.com>
2022-05-26 20:21:38 +0600

    14th commit

--- src/Pages/Portfolio/Touch.js
+++ src/Pages/Portfolio/Touch.js
@@ -12,9 +12,9 @@ const Touch = () => {
                 <h3 className='text-primary  text-4xl font-bold uppercase'>Contact me</h3>
                 <h2 className='text-xl text-accent'>-Get In Touch-</h2>
             </div>
-           <div class="hero ">
-            <div class="hero-content flex-col lg:flex-row">
-                <img src={contact} class="max-w-sm rounded-lg shadow-2xl" />
+           <div className="hero ">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={contact} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h3>If you have any questions, Feel free to contact me</h3>
                     <h5 className='text-xl'>Name:</h5>

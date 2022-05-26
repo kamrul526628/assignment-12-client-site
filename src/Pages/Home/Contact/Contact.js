7da68447aababbe6a831e82de48a142201aa5811
kamrul526628 <kamrulhasan526628@hmail.com>
2022-05-26 20:09:27 +0600

    1o commit

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -10,18 +10,18 @@ const Contact = () => {
            <h1 className='text-center text-4xl text-accent pb-6 '>Contact Us</h1>
            <div className='grid sm:grid-cols-1 lg:grid-cols-2 gap-10'>
             <div>
-                <img src={contact} class="w-3/4"/>
+                <img src={contact} className="w-3/4"/>
             </div>
             <div>
                 <form>
                     
                     
-                    <input type="text" placeholder="Your Name" class="input input-bordered input-sm w-full max-w-xs mb-5" /><br/>
+                    <input type="text" placeholder="Your Name" className="input input-bordered input-sm w-full max-w-xs mb-5" /><br/>
                     
-                    <input type="email" placeholder="Your Email" class="input input-bordered input-sm w-full max-w-xs mb-5" /><br/>
+                    <input type="email" placeholder="Your Email" className="input input-bordered input-sm w-full max-w-xs mb-5" /><br/>
                     
-                    <input type="text" placeholder="Your Message" class="input input-bordered input-md w-full max-w-xs mb-5" /><br/>
-                    <button class="btn btn-wide btn-accent">Send Message</button>
+                    <input type="text" placeholder="Your Message" className="input input-bordered input-md w-full max-w-xs mb-5" /><br/>
+                    <button className="btn btn-wide btn-accent">Send Message</button>
                    
                 </form>
             </div>

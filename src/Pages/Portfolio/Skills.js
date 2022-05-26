baa2dd7c81a31c16bb9d9bdadf847c75217e9762
kamrul526628 <kamrulhasan526628@hmail.com>
2022-05-26 20:19:48 +0600

    13 commit

--- src/Pages/Portfolio/Skills.js
+++ src/Pages/Portfolio/Skills.js
@@ -10,38 +10,38 @@ const Skills = () => {
                 <h3 className='text-primary  text-4xl font-bold uppercase'>My skills</h3>
                 <h2 className='text-xl text-accent'>-What I Know-</h2>
             </div>
-            <div class="hero">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <div class="text-center lg:text-left">
+            <div className="hero">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
                         <div className='flex justify-between'>
                             <h4>HTML</h4>
                             <h4>90%</h4>
                         </div>
-                        <progress class="progress progress-secondary w-96" value="90" max="100"></progress><br/>
+                        <progress className="progress progress-secondary w-96" value="90" max="100"></progress><br/>
                         <div className='flex justify-between'>
                             <h4>CSS</h4>
                             <h4>80%</h4>
                         </div>
-                        <progress class="progress progress-secondary w-96" value="80" max="100"></progress><br/>
+                        <progress className="progress progress-secondary w-96" value="80" max="100"></progress><br/>
                         <div className='flex justify-between'>
                             <h4>JS</h4>
                             <h4>80%</h4>
                         </div>
-                        <progress class="progress progress-secondary w-96" value="80" max="100"></progress><br/>
+                        <progress className="progress progress-secondary w-96" value="80" max="100"></progress><br/>
                         <div className='flex justify-between'>
                             <h4>React</h4>
                             <h4>70%</h4>
                         </div>
-                        <progress class="progress progress-secondary w-96" value="70" max="100"></progress><br/>
+                        <progress className="progress progress-secondary w-96" value="70" max="100"></progress><br/>
                         <div className='flex justify-between'>
                             <h4>Node Js</h4>
                             <h4>60%</h4>
                         </div>
-                        <progress class="progress progress-secondary w-96" value="60" max="100"></progress><br/>
+                        <progress className="progress progress-secondary w-96" value="60" max="100"></progress><br/>
                     </div>
-                    <div class="text-center lg:text-left">
-                        <h1 class="text-4xl font-bold">My creative skills & experiences.</h1>
-                        <p class="py-6">I am a Programmer having years of experience in full stack web development, WordPress development and graphic designing. We are team of highly skilled full stack web developers as well. I can design branding for your startup and also re-design your branding.</p>
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-4xl font-bold">My creative skills & experiences.</h1>
+                        <p className="py-6">I am a Programmer having years of experience in full stack web development, WordPress development and graphic designing. We are team of highly skilled full stack web developers as well. I can design branding for your startup and also re-design your branding.</p>
                     </div>
                     
                 </div>
